f2bdd9da005aed4f36b9c12945d4bd0ce1795895
0411ameya <0411ameya@gmail.com>
2016-11-17 20:01:55 -0500

    Add files via upload

--- proc.sql
+++ proc.sql
@@ -1,6 +1,6 @@
-CREATE or replace PROCEDURE getBookedTicketsProcedure (input_flight_num IN varchar2 , input_flight_date IN date,num_tickets_booked OUT Number) AS
-   
-   BEGIN
-      select count(*) into num_tickets_booked from Reservation_detail where flight_number = input_flight_num and flight_date = input_flight_date;
-   END;
+create or replace procedure getPriceChangeProc (new in number, old in number, change_in_cost out number)
+IS
+BEGIN
+  change_in_cost := new-old;
+END;
 /
