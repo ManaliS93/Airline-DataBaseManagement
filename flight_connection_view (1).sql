2fa8a414f393b22a63988461edc000ab833a6830
0411ameya <0411ameya@gmail.com>
2016-11-29 19:58:32 -0500

    Delete flight_connection_view (1).sql

--- flight_connection_view (1).sql
+++
@@ -1,2 +0,0 @@
-DROP VIEW Flight_connection;
-CREATE VIEW Flight_connection AS SELECT distinct f1.flight_number as flight1, f2.flight_number as flight2,f1.departure_city as city1,f1.arival_city as city2,f2.arival_city as city3 FROM Flight f1, Flight f2 where f1.arival_city = f2.departure_city;
